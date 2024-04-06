1630f3c0a0cc9e2c0b2173394eec70d9d09f2c2c
junior <junior_silvapereira@hotmail.com>
2024-04-06 17:57:33 -0300

    dto

--- go.mod
+++ go.mod
@@ -2,9 +2,8 @@ module initial
 
 go 1.22.2
 
-require github.com/google/uuid v1.6.0
-
 require (
-	golang.org/x/mod v0.17.0 // indirect
-	golang.org/x/tools v0.20.0 // indirect
+	github.com/confluentinc/confluent-kafka-go v1.9.2
+	github.com/devfullcycle/imersao13/go v0.0.0-20240126210247-8ed67ea8931e
+	github.com/google/uuid v1.6.0
 )
--- internal/market/entity/order.go
+++ internal/market/entity/order.go
@@ -12,20 +12,13 @@ type Order struct {
 	Transactions  []*Transaction
 }
 
-func NewOrder(orderID string,
-	investor *Investor,
-	asset *Asset,
-	shares int,
-	price float64,
-	pendingShares int,
-	orderType string,
-	status string) *Order {
+func NewOrder(orderID string, investor *Investor, asset *Asset, shares int, price float64, orderType string) *Order {
 	return &Order{
 		ID:            orderID,
 		Investor:      investor,
 		Asset:         asset,
 		Shares:        shares,
-		PendingShares: pendingShares,
+		PendingShares: shares,
 		Price:         price,
 		OrderType:     orderType,
 		Status:        "OPEN",
